491fdf336a29a64630959aa9f51386127f127993
John Freeman <john.freeman.opensource@gmail.com>
2018-09-24 13:48:34 +0000

    Implemented intellij_jdks role
    Role to configure JDKs in IntelliJ.

---
+++ molecule/default/tests/test_role.py
@@ -0,0 +1,21 @@
+import os
+
+import pytest
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('file_path,expected_text', [
+    ('jdk.table.xml', '/usr/lib/jvm/java-1.8.0-openjdk'),
+    ('jdk.table.xml', '/opt/java/jdk-10/jdk-10.0.1+10'),
+    ('project.default.xml', 'project-jdk-name="1.8"'),
+])
+def test_config_files(host, file_path, expected_text):
+    config_dir_pattern = ('\\.(IdeaIC|IntelliJIdea)[0-9]+\\.[0-9]'
+                          '/config/options$')
+    config_home = host.check_output('find %s | grep --color=never -E %s',
+                                    '/home/test_usr',
+                                    config_dir_pattern)
+    assert host.file(config_home + '/' + file_path).contains(expected_text)
